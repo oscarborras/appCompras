5edb92c7fbf5e8c8a7538d17e607040f1e883c93
oscarborras <oscarborras@iesjulioverne.es>
2024-02-05 13:51:21 +0100

    añadido router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProveedoresComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicioComponent },
+    { path: 'proveedores', component: ProveedoresComponent },
+    { path: '**', component: InicioComponent}
+];
